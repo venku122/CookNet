fef8f767f9a60248b52901aa88bc2bacc585f636
T.J. Tarazevits <tarazevits@gmail.com>
2019-01-29 23:50:11 -0700

    Material UI Text Input Demo

--- frontend/src/components/AddIngredientsInput.js
+++ frontend/src/components/AddIngredientsInput.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
+import '../styles/AddIngredientsInput.css';
 
 class AddIngredientsInput extends Component {
 
   render() {
     return (
       <div>
-        <input type="text" id="ingredientName"/>
-        <input type="text" id="ingredientUnit" />
-        <input type="number" id="ingredientQuantity" />
+        <label htmlFor="inp" className="inp">
+          <input type="text" id="inp" placeholder="&nbsp;"/>
+          <span className="label">Name of Ingredient</span>
+          <span className="border"></span>
+        </label>
+        <input type="text" id="ingredientName" placeholder="Name of Ingredient" />
+        <input type="number" id="ingredientQuantity"placeholder="How much?" />
+        <input type="text" id="ingredientUnit" placeholder="Unit" />
       </div>
     );
   }
